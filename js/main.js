bca2e4ba9c026c7377c412be26f23112a44e3f6d
inae0101 <inaa0118@naver.com>
2022-03-23 08:52:26 +0900

    0323 anime

--- js/main.js
+++ js/main.js
@@ -8,3 +8,43 @@ btnCall.onclick = function(e){
     menuMo.classList.toggle("on"); 
 
 }
+
+const btnOpen = document.querySelector("img"); 
+const aside = document.querySelector("aside"); 
+const _top = aside.querySelector(".top"); 
+const _right = aside.querySelector(".right"); 
+const _bottom = aside.querySelector(".bottom"); 
+const _left = aside.querySelector(".left");
+
+btnOpen.addEventListener("click",e=>{
+    e.preventDefault(); 
+
+    aside.style.display = "block";
+
+    new Anime(_top,{
+        prop:"width", 
+        value: "100%", 
+        duration: 800, 
+        callback :()=>{
+            new Anime(_right,{
+                prop: "height", 
+                value :"100%", 
+                duration: 800, 
+                callback :()=>{
+                    new Anime(_bottom,{
+                        prop:"width", 
+                        value:"100%", 
+                        duration:800, 
+                        callback:()=>{
+                            new Anime(_left, {
+                                prop:"height", 
+                                value:"100%", 
+                                duration:800     
+                            })
+                        }
+                    })
+                }
+            })
+        }
+    })
+});
