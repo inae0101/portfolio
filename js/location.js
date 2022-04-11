a000950ce6f9084ac17c9fef38aaa8c3171bf232
inae0101 <inaa0118@naver.com>
2022-04-12 07:15:27 +0900

    location 수정

--- js/location.js
+++ js/location.js
@@ -1,47 +1,48 @@
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng(37.5251644, 126.9255491), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+	mapOption = {
+		center: new kakao.maps.LatLng(37.5251644, 126.9255491), // 지도의 중심좌표
+		level: 3, // 지도의 확대 레벨
+	};
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption); 
+var map = new kakao.maps.Map(mapContainer, mapOption);
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-var imageSrc= 'img/map marker.png' // 마커이미지의 주소입니다    
-    imageSize = new kakao.maps.Size(50, 52), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(50, 52)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-      
+var imageSrc = 'img/map marker.png'; // 마커이미지의 주소입니다
+(imageSize = new kakao.maps.Size(50, 52)), // 마커이미지의 크기입니다
+	(imageOption = { offset: new kakao.maps.Point(50, 52) }); // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
 // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-    markerPosition = new kakao.maps.LatLng(37.5251644, 126.9255491); // 마커가 표시될 위치입니다
+	markerPosition = new kakao.maps.LatLng(37.5251644, 126.9255491); // 마커가 표시될 위치입니다
 
 // 마커를 생성합니다
 var marker = new kakao.maps.Marker({
-    position: markerPosition, 
-    image: markerImage // 마커이미지 설정 
+	position: markerPosition,
+	image: markerImage, // 마커이미지 설정
 });
 
 // 마커가 지도 위에 표시되도록 설정합니다
-marker.setMap(map);  
+marker.setMap(map);
 
 // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
-var iwContent = '<div style="padding:5px;font: bold 14px Merriweather;">CLAUDEL DESIGN</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+var iwContent =
+	'<div style="padding:5px;font: bold 14px Merriweather;">CLAUDEL DESIGN</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
 
 // 인포윈도우를 생성합니다
 var infowindow = new kakao.maps.InfoWindow({
-    content : iwContent
+	content: iwContent,
 });
 
 // 마커에 마우스오버 이벤트를 등록합니다
-kakao.maps.event.addListener(marker, 'mouseover', function() {
-  // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-    infowindow.open(map, marker);
+kakao.maps.event.addListener(marker, 'mouseover', function () {
+	// 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+	infowindow.open(map, marker);
 });
 
 // 마커에 마우스아웃 이벤트를 등록합니다
-kakao.maps.event.addListener(marker, 'mouseout', function() {
-    // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-    infowindow.close();
+kakao.maps.event.addListener(marker, 'mouseout', function () {
+	// 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+	infowindow.close();
 });
